3cc7d60f60abeef8148016d0247b90790d83cb59
victor-villca <villcavictorleon@gmail.com>
2024-06-20 11:01:16 -0400

    test: pacman collision with spheres score system tests

---
+++ app/src/test/java/com/pac_man/characters/ScoreTests/PacmanSphereScoreTests.java
@@ -0,0 +1,82 @@
+package com.pac_man.characters.ScoreTests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import com.pac_man.Collisions.Nature;
+import com.pac_man.characters.Geometry.Position;
+import com.pac_man.characters.Pacman.Pacman;
+import com.pac_man.characters.Utils.Sprite;
+
+public class PacmanSphereScoreTests {
+    @Test
+    public void testInitialScore() {
+        // define inputs and outputs
+        String expectedScoreText =  "Score: 0";
+        int expectedScoreAmount = 0;
+        Position pos = new Position(0, 0);
+        Pacman pacman = new Pacman(pos, Sprite.RIGHT);
+
+        //execute
+        int actualScoreAmount =pacman.getScore().getAmountPoints();
+        String actualScoreText = pacman.getScore().getText().getText();
+
+        //verify
+        assertEquals(expectedScoreAmount, actualScoreAmount);
+        assertEquals(expectedScoreText, actualScoreText);
+    }
+
+    @Test
+    public void testScoreIncrease() {
+        //definde inputs and outputs
+        String expectedScoreText =  "Score: 10";
+        int expectedScoreAmount = 10;
+        Position pos = new Position(0, 0);
+        Pacman pacman = new Pacman(pos, Sprite.RIGHT);
+
+        //execute
+        pacman.handleCollision(new String[]{"Sphere"}, Nature.WITH);
+        int actualScoreAmount =pacman.getScore().getAmountPoints();
+        String actualScoreText = pacman.getScore().getText().getText();
+        
+        //verify
+        assertEquals(expectedScoreAmount, actualScoreAmount);
+        assertEquals(expectedScoreText, actualScoreText);
+    }
+
+    @Test
+    public void testMultipleSphereCollisions() {
+        //define inputs and outputs
+        String expectedScoreText =  "Score: 50";
+        int expectedScoreAmount = 50;
+        Position pos = new Position(0, 0);
+        Pacman pacman = new Pacman(pos, Sprite.RIGHT);
+
+        //execute
+        pacman.handleCollision(new String[]{"Sphere", "Sphere", "Sphere", "Sphere", "Sphere"}, Nature.WITH);
+        int actualScoreAmount =pacman.getScore().getAmountPoints();
+        String actualScoreText = pacman.getScore().getText().getText();
+
+        //verify
+        assertEquals(expectedScoreAmount, actualScoreAmount);
+        assertEquals(expectedScoreText, actualScoreText);
+    }
+
+    @Test
+    public void testCollisionWithNonSphere() {
+        //define inputs and outputs
+        String expectedScoreText =  "Score: 0";
+        int expectedScoreAmount = 0;
+        Position pos = new Position(0, 0);
+        Pacman pacman = new Pacman(pos, Sprite.RIGHT);
+
+        //execute
+        pacman.handleCollision(new String[]{"Wall"}, Nature.WITH);
+        int actualScoreAmount =pacman.getScore().getAmountPoints();
+        String actualScoreText = pacman.getScore().getText().getText();
+
+        //verify
+        assertEquals(expectedScoreAmount, actualScoreAmount);
+        assertEquals(expectedScoreText, actualScoreText);
+    }
+}
